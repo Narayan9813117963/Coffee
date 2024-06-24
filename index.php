1099333224c818c857c7f37e72dcc565537187c3
Narayan9813117963 <chaudharynarayan799@gmail.com>
2024-06-24 18:26:38 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,165 @@
+<?php include ('inc/header.php'); ?>
+<main>
+    <div class="custom-container banner">
+        <div class="row align-items-center mt-4">
+            <div class="col-lg-4 col-md-12">
+                <div class="left-sec-banner">
+                    <h1>Your coffee Your vibe Your energy</h1>
+                    <p class="banner-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in
+                        libero risus semper habitant arcu eget. Et integer facilisi eget.</p>
+                    <a class="button-primary" href="#">Explore</a>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-12">
+                <div class="hero-image d-flex justify-content-end">
+                    <figure>
+                        <img src="../images/Hero images.png" alt="hero-img">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<section>
+    <div class="product-wrapper position-relative">
+        <div class="custom-container-sm customer-num-detail-wrapper position-absolute">
+            <div class="branch-customer-detail d-flex justify-content-center align-items-center">
+                <div class="row w-100">
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="detail-card-wrapper">
+                            <div class="section-detail d-flex flex-column align-items-center text-center">
+                                <figure class="coffee-icon d-flex justify-content-center align-items-center">
+                                    <img src="../images/icons/coffee.png" alt="">
+                                </figure>
+                                <h3>190+</h3>
+                                <p class="number-detail">Lorem ipsum dolor sit amet, consectetur</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="detail-card-wrapper">
+                            <div class="section-detail d-flex flex-column align-items-center text-center">
+                                <figure class="coffee-icon d-flex justify-content-center align-items-center">
+                                    <img src="../images/icons/coffee.png" alt="">
+                                </figure>
+                                <h3>190+</h3>
+                                <p class="number-detail">Lorem ipsum dolor sit amet, consectetur</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="detail-card-wrapper">
+                            <div class="section-detail d-flex flex-column align-items-center text-center">
+                                <figure class="coffee-icon d-flex justify-content-center align-items-center">
+                                    <img src="../images/icons/coffee.png" alt="">
+                                </figure>
+                                <h3>190+</h3>
+                                <p class="number-detail">Lorem ipsum dolor sit amet, consectetur</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="custom-container container">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
+                    <div class="product-1">
+                        <figure class="product-img">
+                            <img src="../images/shop/product1.jpg" alt="product-1">
+                        </figure>
+                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
+                    <div class="product-1">
+                        <figure class="product-img">
+                            <img src="../images/shop/product2.jpg" alt="product-2">
+                        </figure>
+                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
+                    <div class="product-1">
+                        <figure class="product-img">
+                            <img src="../images/shop/product3.jpg" alt="product-3">
+                        </figure>
+                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
+                    <div class="product-1">
+                        <figure class="product-img">
+                            <img src="../images/shop/product4.jpg" alt="product-4">
+                        </figure>
+                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
+                    <div class="product-1">
+                        <figure class="product-img">
+                            <img src="../images/shop/product5.jpg" alt="product-5">
+                        </figure>
+                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="contact-wrapper mt-4">
+        <div class="custom-container container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="contact-head">
+                        <h2>Contact us</h2>
+                        <p>Please complete te form, and a Cielo representative will contact you promptly!</p>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="contact-form-wrapper">
+                        <form action="submit">
+                            <div class="form-input-wrapper d-flex flex-column">
+                                <div class="contact-step-indicator d-flex justify-content-between">
+                                    <div class="active indicator"></div>
+                                    <div class="indicator"></div>
+                                    <div class="indicator"></div>
+                                </div>
+                                <div class="main-input-wrapper d-flex flex-column gap-3">
+                                    <label for="first-name">*Name</label>
+                                    <div class="first-last-name d-flex gap-4">
+                                        <input type="text" id="first-name" placeholder="FirstName">
+                                        <input type="text" placeholder="LastName">
+                                    </div>
+                                </div>
+                                <div class="main-input-wrapper d-flex flex-column gap-3">
+                                    <label for="email">*Email</label>
+                                    <input type="text" id="email" placeholder="email@example.com">
+                                </div>
+                                <div class="main-input-wrapper d-flex flex-column gap-3">
+                                    <label for="country">*Country</label>
+                                    <select class="form-select" id="country" aria-label="Default select example">
+                                        <option selected>Select</option>
+                                        <option value="1">Nepal</option>z
+                                        <option value="2">India</option>
+                                        <option value="3">China</option>
+                                    </select>
+                                </div>
+                                <div class="main-input-wrapper d-flex flex-column gap-3">
+                                    <label for="phone">*Phone</label>
+                                    <input type="text" id="phone" placeholder="(555) 555-5555">
+                                </div>
+                                <div class="contact-btn">
+                                    <a href="#" class="button-primary">Next</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include ('inc/footer.php'); ?>
