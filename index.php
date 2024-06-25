d43d83cbbbe1133a0c05bbdaba46c21ee415afac
Narayan9813117963 <chaudharynarayan799@gmail.com>
2024-06-25 15:37:56 +0545

    padding reduced in product section

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
                         <figure class="product-img">
                             <img src="../images/shop/product1.jpg" alt="product-1">
                         </figure>
-                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                        <p class="product-name text-center">Et integer facilisi eget</p>
                     </div>
                 </div>
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
@@ -69,7 +69,7 @@
                         <figure class="product-img">
                             <img src="../images/shop/product2.jpg" alt="product-2">
                         </figure>
-                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                        <p class="product-name text-center">Et integer facilisi eget</p>
                     </div>
                 </div>
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
@@ -77,7 +77,7 @@
                         <figure class="product-img">
                             <img src="../images/shop/product3.jpg" alt="product-3">
                         </figure>
-                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                        <p class="product-name text-center">Et integer facilisi eget</p>
                     </div>
                 </div>
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
@@ -85,7 +85,7 @@
                         <figure class="product-img">
                             <img src="../images/shop/product4.jpg" alt="product-4">
                         </figure>
-                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                        <p class="product-name text-center">Et integer facilisi eget</p>
                     </div>
                 </div>
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl d-flex justify-content-center mb-4">
@@ -93,7 +93,7 @@
                         <figure class="product-img">
                             <img src="../images/shop/product5.jpg" alt="product-5">
                         </figure>
-                        <p class="product-name text-center mt-3">Et integer facilisi eget</p>
+                        <p class="product-name text-center">Et integer facilisi eget</p>
                     </div>
                 </div>
             </div>
